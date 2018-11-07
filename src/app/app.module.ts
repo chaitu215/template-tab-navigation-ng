d512af80d2bf28bf1b8dcea553acdeb5e35055a6
Manol Donev <manol.donev@gmail.com>
2018-11-07 18:56:59 +0200

    rework implementation for ns-angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
-import { AppRoutingModule, COMPONENTS } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
 
 @NgModule({
     bootstrap: [
@@ -11,12 +10,10 @@ import { CoreModule } from "./core/core.module";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule,
-        CoreModule
+        AppRoutingModule
     ],
     declarations: [
-        AppComponent,
-        ...COMPONENTS
+        AppComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
